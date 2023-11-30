6cd89dbcd4c6870304d37738443a7dd10641ac5f
ymeron <yoav.meron11@gmail.com>
2023-11-30 14:42:35 +0200

    Updated function for piece creation for more automation.

--- Backend/Gameplay.py
+++ Backend/Gameplay.py
@@ -1,6 +1,7 @@
 from Rules.RunnerRules import RunnerRules
 from Rules.AttackingRules import AttackingRules
 from Rules.MovementRules import MovementRules
+from universals import strength_to_name_and_number_dict as s_to_n_and_n
 from GameBoard import GameBoard
 from Piece import Piece
 
@@ -16,28 +17,12 @@ class Game:
     # Function takes a color as a parameter and returns the initial list of pieces set with that color.
     @staticmethod
     def create_pieces_lists(color):
-        piece_list = [Piece('F', color, "Flag"), Piece(1, color, "Spy"), Piece(10, color, "Marshall"),
-                      Piece(9, color, "General")]
-
-        for i in range(2):
-            piece_list.append(Piece(8, color, "Colonel"))
+        piece_list = [Piece('F', color, "Flag")]
+        for i in range(1, 11):
+            for j in range(s_to_n_and_n[i][1]):
+                piece_list.append(Piece(i, color, s_to_n_and_n[i][0]))
 
         for i in range(3):
-            piece_list.append(Piece(7, color, "Major"))
-
-        for i in range(4):
-            piece_list.append(Piece(6, color, "Captain"))
-
-        for i in range(4):
-            piece_list.append(Piece(5, color, "Lieutenant"))
-
-        for i in range(4):
-            piece_list.append(Piece(4, color, "Sergeant"))
-
-        for i in range(5):
-            piece_list.append(Piece(3, color, "Miner"))
-
-        for i in range(8):
-            piece_list.append(Piece(2, color, "Scout"))
+            piece_list.append(Piece('B', color, "Bomb"))
 
         return piece_list
