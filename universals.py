a77595e35b3cacc98209de0cb893edb073d7dc36
ymeron <yoav.meron11@gmail.com>
2023-11-30 14:46:12 +0200

    Created file for universal objects. Created dictionary that associates a pieces rank with its name and amount on the board.

---
+++ universals.py
@@ -0,0 +1,3 @@
+strength_to_name_and_number_dict = {'F': "Flag", 'B': "Bomb", 1: ["Spy", 1], 2: ["Scout", 8], 3: ["Miner", 5],
+                                    4: ["Sergeant", 4], 5: ["Lieutenant", 4], 6: ["Captain", 4], 7: ["Major", 3],
+                                    8: ["Colonel", 2], 9: ["General", 1], 10: ["Marshall", 1]}
