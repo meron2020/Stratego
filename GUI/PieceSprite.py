36b6f13121977991ad95cd707eb23a49e99d32cb
ymeron <yoav.meron11@gmail.com>
2023-11-30 14:44:16 +0200

    Added padding needed for centering the sprites on the cells in the middle of the screen.

--- GUI/PieceSprite.py
+++ GUI/PieceSprite.py
@@ -5,15 +5,17 @@ class PieceSprite(pygame.sprite.Sprite):
     def __init__(self, image, row, column, board):
         super().__init__()
         self.board = board
-        self.image = pygame.transform.scale(pygame.image.load(image), (20, 20))
+        self.image = pygame.transform.scale(pygame.image.load(image),
+                                            (int(self.board.cell_size), int(self.board.cell_size)))
         self.rect = self.image.get_rect()
         self.rect.topleft = self.calculate_exact_position(row, column)
 
     def calculate_exact_position(self, row, column):
-        x_pos = row * (self.board.height + self.board.margin)
-        y_pos = column * (self.board.height + self.board.margin)
+        start_x = (pygame.display.get_window_size()[0] - 10 * (self.board.cell_size + self.board.margin)) // 2
+        start_y = (pygame.display.get_window_size()[1] - 10 * (self.board.cell_size + self.board.margin)) // 2
+        x_pos = start_x + row * (self.board.cell_size + self.board.margin) + self.board.margin
+        y_pos = start_y + column * (self.board.cell_size + self.board.margin) + self.board.margin
         return tuple((x_pos, y_pos))
 
     def set_new_pos(self, row, column):
         self.rect.topleft = self.calculate_exact_position(row, column)
-
