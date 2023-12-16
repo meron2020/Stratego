7e2522ad27ff26f9bb2bf387c3d1549dd89b111d
yoavm <yoav.meron11@gmail.com>
2023-12-16 21:50:59 +0200

    Changed few function names and added delete piece funcion.

--- Backend/GameFunctionality/GameBoard.py
+++ Backend/GameFunctionality/GameBoard.py
@@ -9,14 +9,14 @@ class GameBoard:
             for j in range(10):
                 self._board_matrix[i][j] = None
 
-    def get_piece_in_position(self, position):
+    def get_piece_id_in_position(self, position):
         return self._board_matrix[position[0]][position[1]]
 
     def get_board_matrix(self):
         return self._board_matrix
 
     # Moves a piece to a new position in the board matrix.
-    def set_new_piece_position(self, piece, new_position):
+    def set_new_piece_id_position(self, piece, new_position):
         # Finds piece position and changes it, only if game has already started.
         if piece.position:
             piece_pos = piece.position
@@ -24,8 +24,11 @@ class GameBoard:
 
         # Sets piece position to new position and changes board matrix accordingly.
         piece.position = new_position
-        self._board_matrix[new_position[0]][new_position[1]] = piece
+        self._board_matrix[new_position[0]][new_position[1]] = piece.id
         return True
+
+    def delete_piece(self, position):
+        self._board_matrix[position[0]][position[1]] = None
 
     def get_piece_count(self):
         piece_count = 0
