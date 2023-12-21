c555118beef3ea49532d40131d77f813f199b80d
ymeron <yoav.meron11@gmail.com>
2023-12-21 14:41:30 +0200

    Changed process id to client return address.

--- Backend/RequestQueuePacket/RequestPacket.py
+++ Backend/RequestQueuePacket/RequestPacket.py
@@ -1,7 +1,7 @@
 class RequestPacket:
-    def __init__(self, game_id, request_type, data, http_process_id, player_color):
+    def __init__(self, game_id, request_type, data, client_address, player_color):
         self.game_id = game_id
-        self.process_id = http_process_id
+        self.return_address = client_address
         self.data = data
         self.request_type = request_type
         self.player_color = player_color
