c2683dc2517f69f0f71550b28ec9cf1fc57847d2
yoavm <yoav.meron11@gmail.com>
2023-12-27 15:02:07 +0200

    Continued Development

--- Backend/GameHandlerAPI/GameHandler/GameHandler.py
+++ Backend/GameHandlerAPI/GameHandler/GameHandler.py
@@ -2,9 +2,10 @@ from Backend.GameFunctionality.Game import Game
 
 
 class GameHandler:
-    def __init__(self, request_queue):
+    def __init__(self, request_queue, response_queue):
         self.games = []
         self.request_queue = request_queue
+        self.response_queue = response_queue
 
     def create_game(self):
         game_id = len(self.games)
@@ -15,7 +16,12 @@ class GameHandler:
     def piece_action(self, game_id, data):
         game = self.games[game_id]
         game.piece_act(data["piece_id"], data["new_pos"])
+        return game.get_board()
 
+    def get_board(self, game_id):
+        game = self.games[game_id]
+        return game.get_board()
 
-
-    
+    def quit_game(self, player_id, game_id):
+        game = self.games[game_id]
+        
