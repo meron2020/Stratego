9114f829da82b8f00ac84de8b699334c930bdd58
ymeron <yoav.meron11@gmail.com>
2024-01-07 12:34:39 +0200

    Moved to new directory.

--- Backend/Queues/RequestPacket.py
+++ Backend/Queues/RequestPacket.py
@@ -1,5 +1,5 @@
 class RequestPacket:
-    def __init__(self, game_id, request_type , data, client_address, player_id):
+    def __init__(self, game_id, request_type, data, client_address, player_id):
         self.game_id = game_id
         self.return_address = client_address
         self.data = data
