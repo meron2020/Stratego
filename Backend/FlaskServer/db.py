f9ee95d89bd113a235eee0728cffd466b9b48980
yoavm <yoav.meron11@gmail.com>
2024-05-29 16:30:13 +0300

    Developments

--- Backend/FlaskServer/db.py
+++ Backend/FlaskServer/db.py
@@ -1,3 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# Create an instance of SQLAlchemy to be used with the Flask application.
+# This instance will manage the database connection and provide ORM functionality.
 db = SQLAlchemy()
